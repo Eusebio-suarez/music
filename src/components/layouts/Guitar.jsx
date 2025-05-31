ea669bc884e02ec771f252af9843a4886d5f2b70
eusebio suarez <eusebiosuaresmartines@gmail.com>
2025-05-30 22:43:05 -0500

    correcion de tamaños

--- src/components/layouts/Guitar.jsx
+++ src/components/layouts/Guitar.jsx
@@ -4,16 +4,16 @@ import guitarImg from "../../assets/raptis.png"
 export const Guitar = () => {
   return (
     <div
-      className='w-full flex relative justify-end items-center gap-10 text-white bg-cover bg-center min-h-[190px]'
+      className='w-full flex relative justify-end items-center gap-10 text-white bg-cover bg-center min-h-[230px]'
       style={{ backgroundImage: `url(${guitarImg})` }}
     >
-      <div className='flex gap-1.5 flex-col absolute w-[150px] right-4'>
+      <div className='flex gap-1.5 flex-col absolute w-[200px] right-4'>
         <div className='flex flex-col gap-2'>
           <div>
             <p className='text-[19px] font-semibold'>SONDS VIBES WITH COLORS</p>
           </div>
           <div>
-            <p className='text-[8px] font-semibold'>
+            <p className='text-[11px] font-semibold'>
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, quos officia eius in nam rem mollitia earum eligendi molestias quas, voluptas excepturi aliquid delectus dolore saepe ratione sunt ipsam non?
             </p>
           </div>
--- src/components/layouts/Seccion.jsx
+++ src/components/layouts/Seccion.jsx
@@ -6,17 +6,17 @@ export const Seccion = ({titulo="MUSIC." ,subTitulo="FUN.",subTitulo2="ENTERTAIN
         <div className='flex gap-1 flex-col'>
             <div className='flex flex-col gap-2'>
               <div>
-                <p className='text-[19px] font-semibold bg-clip-text text-transparent bg-gradient-to-r from-[#fd4b57] to[#76204f] via-purple-500'>{titulo}</p>
-                <p className='text-[19PX] font-semibold'>{subTitulo}</p>
-                <p className='text-[19px] font-semibold'>{subTitulo2}</p>
+                <p className='text-[16px] font-semibold bg-clip-text text-transparent bg-gradient-to-r from-[#fd4b57] to[#76204f] via-purple-500'>{titulo}</p>
+                <p className='text-[16PX] font-semibold'>{subTitulo}</p>
+                <p className='text-[16px] font-semibold'>{subTitulo2}</p>
               </div>
               <div>
-                <p className='text-[8.5px] font-semibold'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, quos officia eius in nam rem mollitia earum eligendi molestias quas, voluptas excepturi aliquid delectus dolore saepe ratione sunt ipsam non?</p>
+                <p className='text-[11px] font-semibold'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, quos officia eius in nam rem mollitia earum eligendi molestias quas, voluptas excepturi aliquid delectus dolore saepe ratione sunt ipsam non?</p>
               </div>
             </div>
               <button className='bg-[#f94e60] hover:bg-[#e61837] hover:scale-[103%] transition duration-300 rounded-2xl mt-1  w-[80px] py-1.5 text-[8px] text-white'>GO THESE</button>
         </div>
-        <div className=''>
+        <div className='h-full'>
           <img className='h-full' src={imgSrc} alt="" />
         </div>
     </div>
